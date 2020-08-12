e2a5ba790fb34f1c16015abc0c58f4ecf1df59c7
Oleksii Kuznietsov <oleksiikuz@ukr.net>
2020-08-13 00:44:20 +0300

    new commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,25 @@
-$(function(){
-
-
-
-  
+$('.promo__slider').slick({
+   centerMode: true,
+   centerPadding: '190px',
+   slidesToShow: 1,
+   responsive: [
+      {
+         breakpoint: 768,
+         settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 3
+         }
+      },
+      {
+         breakpoint: 480,
+         settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+         }
+      }
+   ]
 });
